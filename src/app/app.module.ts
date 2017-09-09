2b1da73b384b24732dd351b5f83cda55f69af245
maria1820 <marvalenvia2018@gmail.com>
2017-09-09 15:29:39 -0500

    cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustComponent } from './cust/cust.component';
+import { CustomersComponent } from './customers/customers.component';
+import { AddComponent } from './customers/add/add.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustComponent,
+    CustomersComponent,
+    AddComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
